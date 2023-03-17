69a3c4f7dcce35e5c424b46bceb5721477f0b503
Teddy Bersentes <theodore.bersentes@gmail.com>
2023-03-17 13:26:15 -0700

    Added node version to package.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<Component {...pageProps} />;
+			<Component {...pageProps} />
 		</main>
 	)
 }
