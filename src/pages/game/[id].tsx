50b9d4f182596b248803fb05b1572d0205e58040
Teddy Bersentes <theodore.bersentes@gmail.com>
2023-03-26 21:07:55 -0400

    wip

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -23,7 +23,7 @@ export default function Game() {
 	}, [])
 
 	return (
-		<div className='relative flex items-center justify-center w-screen h-[100svh] bg-cowboy-gray-50 touch-none'>
+		<div className='relative flex items-center justify-center w-screen h-[100svh] bg-cowboy-gray-50 touch-none game-mobile-touch'>
 			{userId && gameId && <GameLoader userId={userId} gameId={gameId} />}
 			<RegisterPopup
 				isOpen={userPopup}
